3005030a4693e8db432f8677626c6ad37cead067
Daniel Ojo <ojoxdan@gmail.com>
2021-09-28 05:29:44 +0100

    new commit

---
+++ build/precache-manifest.3461f5d62ba327b7d87298a64fb831a3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86caa8de5ba89cf9051256594355fa72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eb8ad43a235c4464450",
+    "url": "/static/css/main.e2e1491c.chunk.css"
+  },
+  {
+    "revision": "3142b097737a43c0fd8c",
+    "url": "/static/js/2.666dc6a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.666dc6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb8ad43a235c4464450",
+    "url": "/static/js/main.5976f550.chunk.js"
+  },
+  {
+    "revision": "bf9ced60b11b4cb62013",
+    "url": "/static/js/runtime-main.73c86988.js"
+  },
+  {
+    "revision": "0d63739513944b1e7ca6f19980a57689",
+    "url": "/static/media/desktop-only.0d637395.PNG"
+  },
+  {
+    "revision": "c8b8122c1cb4f117713f5fef7da31b04",
+    "url": "/static/media/desktop-tablet.c8b8122c.PNG"
+  },
+  {
+    "revision": "09a8e84fe2bdb69582b24527750d3305",
+    "url": "/static/media/dummy.09a8e84f.PNG"
+  },
+  {
+    "revision": "ae1e00023a7fa2deebddb58d4ee78a06",
+    "url": "/static/media/featured.ae1e0002.PNG"
+  },
+  {
+    "revision": "09fd07522dac3656648c37a72af034bd",
+    "url": "/static/media/file.09fd0752.PNG"
+  },
+  {
+    "revision": "5fa4b749033cc71ca8f77acd1dab1510",
+    "url": "/static/media/green-graph.5fa4b749.PNG"
+  },
+  {
+    "revision": "a0188f3d0246b062669657b4f842166d",
+    "url": "/static/media/hero.a0188f3d.PNG"
+  },
+  {
+    "revision": "516ec34b6cb427abd4ba77acf41e6174",
+    "url": "/static/media/scam-alert.516ec34b.PNG"
+  },
+  {
+    "revision": "1afb14d2cc4cafc1bf2cfa6aa0c4f183",
+    "url": "/static/media/shield.1afb14d2.PNG"
+  },
+  {
+    "revision": "60c6703997f2d805253ee567cbe1d9ef",
+    "url": "/static/media/skyscraper.60c67039.PNG"
+  }
+]);
--- coming-soon-build/service-worker.js
+++ coming-soon-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3461f5d62ba327b7d87298a64fb831a3.js"
+  "./precache-manifest.05dd8b0d771af63e7fbf26db11e5c91c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
